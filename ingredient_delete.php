c81627f5d36a7a588db265f63e68728740942a16
CryHavok01 <71348370+CryHavok01@users.noreply.github.com>
2021-01-27 14:58:28 -0500

    Create ingredient_delete.php

---
+++ ingredient_delete.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'dbh.php';
+$stmt = $connection->prepare("DELETE FROM spices WHERE spice_num=?;");
+$stmt->bind_param("i", $idNum);
+$idNum = $_POST['spice_num'];
+$stmt->execute();
+$stmt->close();
+header("location: index.php");
+?>
