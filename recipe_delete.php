e807edc48ee5af8941f1cfe1cc6dd83ac00fc754
CryHavok01 <71348370+CryHavok01@users.noreply.github.com>
2021-01-27 15:02:16 -0500

    Create recipe_delete.php

---
+++ recipe_delete.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'dbh.php';
+$stmt = $connection->prepare("DELETE FROM recipe_names WHERE recipe_id=?;");
+$stmt->bind_param("i", $idNum);
+$idNum = $_POST['recipe_id'];
+$stmt->execute();
+$stmt->close();
+$stmt = $connection->prepare("DELETE FROM recipe_ingredients WHERE recipe_id=?;");
+$stmt->bind_param("i", $idNum);
+$idNum = $_POST['recipe_id'];
+$stmt->execute();
+$stmt->close();
+$stmt = $connection->prepare("DELETE FROM recipe_steps WHERE recipe_id=?;");
+$stmt->bind_param("i", $idNum);
+$idNum = $_POST['recipe_id'];
+$stmt->execute();
+$stmt->close();
+header("location: index.php");
+?>
